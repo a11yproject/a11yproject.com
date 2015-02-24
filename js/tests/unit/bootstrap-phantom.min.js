a3a4738bdb26ec3a22a1655c48093a922a43e2de
grayghostvisuals <grayghost@grayghostvisuals.com>
2015-02-24 14:37:18 -0500

    bare bones skeleton

--- js/tests/unit/bootstrap-phantom.min.js
+++
@@ -1,3 +0,0 @@
-// Logging setup for phantom integration
-// adapted from Modernizr
-QUnit.begin=function(){console.log("Starting test suite");console.log("================================================\n")};QUnit.moduleDone=function(e){e.failed===0?console.log("✔ All tests passed in '"+e.name+"' module"):console.log("✖ "+e.failed+" tests failed in '"+e.name+"' module")};QUnit.done=function(e){console.log("\n================================================");console.log("Tests completed in "+e.runtime+" milliseconds");console.log(e.passed+" tests of "+e.total+" passed, "+e.failed+" failed.")};
