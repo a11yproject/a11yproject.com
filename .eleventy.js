0f3e81cebf9f4d82b8d1b839b39afb968954d1aa
Eric Bailey <eric.w.bailey@gmail.com>
2020-07-05 18:22:44 -0400

    Update Eleventy to v11

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const { DateTime } = require("luxon");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const moment = require('moment');
 const slugify = require("slugify");
