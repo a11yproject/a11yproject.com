c60d1db3920e63814a44f429d7910a3a7e56b77d
Eric Bailey <eric.w.bailey@gmail.com>
2020-07-12 15:34:25 -0400

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,38 +1,9 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
-  /(?:avatars3\.githubusercontent\.com|\.(?:png|gif|jpg|jpeg|webp|svg)$)/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'images',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60
-      }),
-    ],
-  })
-);
-
-workbox.routing.registerRoute(
   /\.(?:js|css)$/,
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'static-resources'
-  })
-);
-
-workbox.routing.registerRoute(
-  /^https:\/\/fonts\.gstatic\.com/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'google-fonts-webfonts',
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200]
-      }),
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 365,
-        maxEntries: 30
-      })
-    ]
   })
 );
 
