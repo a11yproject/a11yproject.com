8054227ecf52867ea8761ab7e75936b178d489ed
Andrew Borstein <aborstein@wespire.com>
2020-08-02 00:11:33 -0400

    Open checklist item details on permalink navigation
    Automatically open the checklist item <details> when its navigated to using the new permalink URL.

--- src/js/main.js
+++ src/js/main.js
@@ -49,3 +49,22 @@ if (navigator && navigator.clipboard && navigator.permissions) {
 		};
 	});
 }
+
+// When someone navigates directly to a checklist item using a permalink URL
+// like a11yproject.com/checklist/#validate-your-html, the item with the
+// matching id attribute will be scrolled into view. Then, if JS is enabled,
+// this code will open its associated <details> element. 
+function openLinkedCheckListItem() {			
+	var checklistItems = document.querySelectorAll("[data-checklist-item-id]");
+
+	checklistItems.forEach(function (item) {
+		var permalinkHref = "#" + item.getAttribute("data-checklist-item-id");
+		var isItemLinked = permalinkHref === document.location.hash;
+
+		if (isItemLinked) {
+			item.setAttribute("open", true);
+		}
+	});
+}
+
+openLinkedCheckListItem();
