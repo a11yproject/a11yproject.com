54cde040c88ec60b72bfe3aa63f8358ab4037643
Andrew Borstein <aborstein@wespire.com>
2020-08-04 20:52:26 -0400

    Close #1013: Add permalink to checklist items (#1034)
    * Add permalink to checklist item details
    
    Allow website visitors to copy and share a direct link to a specific checklist item.
    
    * Open checklist item details on permalink navigation
    
    Automatically open the checklist item <details> when its navigated to using the new permalink URL.
    
    * Change "Permalink" to "Share Link"
    
    It's a little less jargony and the URL will also change when the title changes, so it's not actually very permanent.
    
    * Use visually hidden text instead of aria to provide share link context
    
    This is a more reliable technique that also respects the first rule of aria.
    
    * Extract checklist item data to variables

--- src/js/main.js
+++ src/js/main.js
@@ -49,3 +49,22 @@ if (navigator && navigator.clipboard && navigator.permissions) {
 		};
 	});
 }
+
+// When someone navigates directly to a checklist item using its "Share Link" 
+// like a11yproject.com/checklist/#validate-your-html, the item with the
+// matching id attribute will be scrolled into view. Then, if JS is enabled,
+// this code will open its associated <details> element. 
+function openLinkedCheckListItem() {			
+	var checklistItems = document.querySelectorAll("[data-checklist-item-id]");
+
+	checklistItems.forEach(function (item) {
+		var shareLink = "#" + item.getAttribute("data-checklist-item-id");
+		var isItemLinked = shareLink === document.location.hash;
+
+		if (isItemLinked) {
+			item.setAttribute("open", true);
+		}
+	});
+}
+
+openLinkedCheckListItem();
