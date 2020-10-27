6b164dd7f6082a4620864221bfa0dda1f293531b
Wayne Elgin <wayne.elgin@gmail.com>
2020-10-26 22:22:56 -0500

    Remove unused email link processing

--- src/js/main.js
+++ src/js/main.js
@@ -1,60 +1,10 @@
 /* jshint esversion: 5 */
 
-// Copy email address ----------------------------------------------------------
-function getEmailOnly(element) {
-	var emailRegex = /^(?:mailto:?)([a-zA-Z0-9_\-.]+@[a-zA-Z0-9-]+\.[a-zA-Z\-.]{2,})(?:\?subject\=.*)?$/;
-	return emailRegex.exec(element.getAttribute("href"))[1];
-}
-
-function getEmailElements() {
-	return document.querySelectorAll("[data-mailto]");
-}
-
-function generateCopyEmailButton(element) {
-	var email = getEmailOnly(element);
-	var newEl = document.createElement("button");
-	newEl.className = "c-copy-email-button";
-	newEl.setAttribute("data-copy-email", true);
-	newEl.setAttribute("type", "button");
-	newEl.addEventListener("click", function(e) {
-		navigator.clipboard.writeText(email).then(function () {
-			console.log('successfully copied: "' + email + '"');
-		}).catch(function (error) {
-			console.error('failed to copy ' + email);
-		});
-	});
-	newEl.innerText = "Copy";
-	element.after(newEl);
-}
-
-function processEmailHrefs() {
-	var emailElements = getEmailElements();
-	emailElements.forEach(generateCopyEmailButton);
-}
-
-if (navigator && navigator.clipboard && navigator.permissions) {
-	navigator.permissions.query({
-		name: 'clipboard-write'
-	}).then(function (permissionStatus) {
-		// Will be 'granted', 'denied' or 'prompt':
-		if (permissionStatus.state === "granted") {
-			processEmailHrefs();
-		}
-
-		// Listen for changes to the permission state
-		permissionStatus.onchange = function () {
-			if (permissionStatus.state === "granted") {
-				processEmailHrefs();
-			}
-		};
-	});
-}
-
-// When someone navigates directly to a checklist item using its "Share Link" 
+// When someone navigates directly to a checklist item using its "Share Link"
 // like a11yproject.com/checklist/#validate-your-html, the item with the
 // matching id attribute will be scrolled into view. Then, if JS is enabled,
-// this code will open its associated <details> element. 
-function openLinkedCheckListItem() {			
+// this code will open its associated <details> element.
+function openLinkedCheckListItem() {
 	var checklistItems = document.querySelectorAll("[data-checklist-item-id]");
 
 	checklistItems.forEach(function (item) {
