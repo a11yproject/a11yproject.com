e8221c494d8e2449f8655a70f0aaeaed15d70d52
EJ Mason <eliasjmason@gmail.com>
2021-04-22 16:06:26 -0700

    Use native Date functionality to do conversions

--- .eleventy.js
+++ .eleventy.js
@@ -20,12 +20,29 @@ module.exports = function (eleventyConfig) {
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd');
+		return dateObj.toISOString();
   });
 
-  eleventyConfig.addFilter('dateReadable', date => {
-    return moment(date).format('LL'); // E.g. May 31, 2019
-  });
+	/**
+	 * Returns a humnan-readable date
+		E.g. May 31, 2019
+	 */
+
+  eleventyConfig.addFilter("dateReadable", (value) => {
+		const date = new Date(value);
+		const utcDate = new Date(
+			date.getUTCFullYear(),
+			date.getUTCMonth(),
+			date.getUTCDate()
+		);
+		const formatOpts = {
+			timezone: "UTC",
+			day: "numeric",
+			month: "long",
+			year: "numeric",
+		};
+		return new Intl.DateTimeFormat("en-US", formatOpts).format(utcDate);
+	});
 
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
