645c1eef78538dce20a62b1e8a645eaf13599634
EJ Mason <13525251+mxmason@users.noreply.github.com>
2021-06-29 11:02:12 -0700

    Automatically convert category to tag

--- src/posts/posts.11tyData.js
+++ src/posts/posts.11tyData.js
@@ -1,9 +1,18 @@
+const ALPHABETICAL_RE = new RegExp("[^a-zA-Z0-9 -]");
+
 module.exports = {
 	layout: "post",
 	eleventyComputed: {
 		eleventyNavigation: {
 			key: (data) => data.title,
 			title: (data) => data.title,
+		},
+		tags: ({ category, tags }) => {
+			if (tags) {
+				return tags;
+			} else {
+				return [category.replace(ALPHABETICAL_RE, "").toLowerCase()];
+			}
 		},
 	},
 };
