d36356d05a4e13daad6e707cbffd246c56731571
EJ Mason <13525251+mxmason@users.noreply.github.com>
2021-06-29 19:48:41 -0700

    Remove computed tags

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,19 +1,9 @@
-// Tags cannot have non-alphabetical characters
-const FORBIDDEN_TAG_CHARS_EXP = new RegExp("[^a-zA-Z]");
-
 module.exports = {
 	layout: "post",
 	eleventyComputed: {
 		eleventyNavigation: {
 			key: ({ category }) => category,
 			title: ({ title }) => title,
-		},
-		tags: ({ category, tags }) => {
-			if (tags) {
-				return tags;
-			} else {
-				return [category.replace(FORBIDDEN_TAG_CHARS_EXP, "").toLowerCase()];
-			}
 		},
 	},
 };
